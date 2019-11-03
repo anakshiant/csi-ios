b274e3dff63ddb7b34a7ad3ccd1cd84d93aad783
Anakshiant <nndchaudhary8@gmail.com>
2019-11-03 23:13:25 +0530

    minor changes

--- Screens/Account/Login.js
+++ Screens/Account/Login.js
@@ -1,19 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import { Input } from "react-native-elements";
+import React, { useContext, useState} from "react";
+import { View, Text, StyleSheet, Image, TextInput } from "react-native";
+import { Input, Button, ThemeConsumer } from "react-native-elements";
+
 
 import { withLayout } from "../../Components/withLayout";
+import AuthContext from "../../Contexts/AuthContext";
+import ThemeContext from "../../Contexts/ThemeContext";
 
-export const Login = () => {
+export const Login = ({navigation}) => {
+  const {PrimaryColor} = useContext(ThemeContext);
+  const {Login} = useContext(AuthContext);
+
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("")
+
   return (
     <View style={styles.main}>
-      <View>
+      <View style={styles.imageView}>
         <Image source={require("../../assets/icon.png")} />
-        <Input placeholder="Email" />
       </View>
       <View style={styles.loginSection}>
-        <Input placeholder="Email" />
-        <Input placeholder="Password" />
+        <TextInput placeholder="Email" style={styles.textInput} onChange={e=>setEmail(e.text)} />
+        <TextInput placeholder="Password" style={styles.textInput} onChange={e=>setPassword(e.text)} />
+        <Button title="Login" type="solid" raised={true} onPress={}/>
+      </View>
+      <View style={styles.signupView}>
+        <Text style={{ fontWeight: "bold" }} onPress={}>
+          New Here? Click here to Register
+        </Text>
       </View>
     </View>
   );
@@ -25,10 +39,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+  imageView: {
+    justifyContent: "center"
+  },
   loginSection: {
     justifyContent: "center",
-    alignItems: "stretch"
+    alignItems: "stretch",
+    padding: 20
+  },
+  textInput: {
+    width: 300,
+    padding: 20
+  },
+  signupView: {
+    justifyContent: "center"
   }
 });
 
-export default Login;
+export default withLayout(Login);
